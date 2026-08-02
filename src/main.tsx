891beb7f37d795d1e105cb735986b45d73b731f4
ultramonkey23 <haringcody@gmail.com>
2026-08-02 12:48:36 -0400

    Load procedural rendering contract after base styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import StudioFieldShell from "./components/StudioFieldShell";
 import "./index.css";
 import "./procedural-shell.css";
+import "./procedural-rendering.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
