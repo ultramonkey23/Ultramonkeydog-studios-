d0a5b175242630a19e7236ca2c381880c8773e94
ultramonkey23 <haringcody@gmail.com>
2026-08-02 03:10:15 -0400

    Load procedural shell styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import StudioFieldShell from "./components/StudioFieldShell";
 import "./index.css";
+import "./procedural-shell.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
