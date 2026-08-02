a5834544781c568b8764a7e3fb6494347b5b0b8e
ultramonkey23 <haringcody@gmail.com>
2026-08-02 03:09:47 -0400

    Mount the procedural studio shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import StudioFieldShell from "./components/StudioFieldShell";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <StudioFieldShell>
+      <App />
+    </StudioFieldShell>
   </StrictMode>,
 );
